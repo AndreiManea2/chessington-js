26dc66c011890817a9d5c5b5fd6db61ff997b05c
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-10-06 13:40:54 +0100

    pawns can move 1 or 2 squares

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -1,14 +1,36 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
 
 export default class Pawn extends Piece {
+    private hasMoved: boolean = false;
+
     public constructor(player: Player) {
         super(player);
     }
 
+    public moveTo(board: Board, newSquare: Square) {
+        super.moveTo(board, newSquare);
+        this.hasMoved = true;
+    }
+
     public getAvailableMoves(board: Board) {
         const currentSquare = board.findPiece(this);
-        return {'col': currentSquare.col, 'row': this.player === Player.WHITE ? currentSquare.row + 1 : currentSquare.row - 1};
+        const direction = this.player === Player.WHITE ? 1 : -1;
+
+        const availableMoves: Square[] = [];
+
+        // Always can move one square forward
+        const oneStep = Square.at(currentSquare.row + direction, currentSquare.col);
+        availableMoves.push(oneStep);
+
+        // If it hasn't moved yet, it can also move two squares forward
+        if (!this.hasMoved) {
+            const twoStep = Square.at(currentSquare.row + 2 * direction, currentSquare.col);
+            availableMoves.push(twoStep);
+        }
+
+        return availableMoves;
     }
 }
