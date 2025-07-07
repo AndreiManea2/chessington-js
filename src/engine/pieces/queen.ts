f52c8045eafaf6c7b39f9c52168503c3efa37523
AndreiManea2 <Andrei.Manea@softwire.com>
2025-07-07 14:03:33 +0300

    queen tests pass

--- src/engine/pieces/queen.ts
+++ src/engine/pieces/queen.ts
@@ -8,6 +8,25 @@ export default class Queen extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const horizontalDirections = [
+            {row: 0, col: 1}, // right
+            {row: 0, col: -1}, // left
+            {row: 1, col: 0}, // down
+            {row: -1, col: 0}, // up
+        ];
+
+        const horizontalMoves = this.getLinearMoves(board, horizontalDirections);
+
+        const diagonalDirections = [
+            {row: 1, col: 1}, // right-up
+            {row: 1, col: -1}, // left-up
+            {row: -1, col: 1}, // right-down
+            {row: -1, col: -1}, // left-down
+        ];
+
+        const diagonalMoves = this.getLinearMoves(board, diagonalDirections);
+
+        return horizontalMoves.concat(diagonalMoves);
+
     }
 }
